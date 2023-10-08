e95dff9e8ab80f649d474f265a921d0832c3add8
miroslaw-mroczkowski <97404193+miroslaw-mroczkowski@users.noreply.github.com>
2023-10-08 22:38:40 +0200

    Finished js, minified css + readme

--- script.js
+++ script.js
@@ -3,11 +3,18 @@
 const form = document.querySelector('.form');
 const btn = document.querySelector('.btnSubmit');
 const email = document.querySelector('.emailInput');
+const emailError = document.querySelector('.emailError');
 
 form.addEventListener('submit', function (e) {
-	console.log('heloł');
-	const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, 'gm');
+  e.preventDefault();
+  const emailRegex = new RegExp(
+    /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/,
+    'gm'
+  );
 
-	const isValidEmail = emailRegex.test(email);
-	console.log(isValidEmail);
+  const isValidEmail = emailRegex.test(email.value);
+
+  isValidEmail
+    ? emailError.classList.add('hidden')
+    : emailError.classList.remove('hidden');
 });
